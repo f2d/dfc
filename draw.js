144a24ce82790de96a3c245ff4d0abbb8191f8d0
MyonMyon <axfesunov@gmail.com>
2013-04-05 05:21:12 +0300

    [v1.1.0] Кислотный апдейт
    Добавлена функция инверсии (по кнопке I).
    Добавлены палитры.
    Номер версии и дата хранятся в js-файле.

--- draw.js
+++ draw.js
@@ -13,9 +13,26 @@ var cWidth = 600;
 var cHeight = 360;
 var color = "0, 0, 0";
 var debugMode = false;
-var palette = ["#000" , "#00a" , "#0a0" , "#0aa" , "#a00" , "#a0a" , "#aa0" , "#aaa" , "#555" , "#55f" , "#5f5" , "#5ff" , "#f55" , "#f5f" , "#ff5" , "#fff"];
+
 var drugMode = false;
 var flushCursor = false;
+var infoVersion = "v1.1.0";
+var infoDate = "April 5, 2013"
+
+var paletteDesc = {"classic" : "Classic", "cga" : "CGA", "win7" : "Шindoшs", "gray" : "Post-Rock", "feijoa1" : "Feijoa-01"};
+var paletteWidth = {"classic" : 8, "cga" : 8, "win7" : 10, "gray" : 16, "feijoa1" : 16};
+var palette = new Array();
+	palette["classic"] = ["#000000" , "#000080" , "#008000" , "#008080" , "#800000" , "#800080" , "#808000" , "#c0c0c0" ,
+		"#808080" , "#0000ff" , "#00ff00" , "#00ffff" , "#ff0000" , "#ff00ff" , "#ffff00" , "#ffffff"];
+	palette["cga"] = ["#000" , "#00a" , "#0a0" , "#0aa" , "#a00" , "#a0a" , "#aa0" , "#aaa" , "#555" , "#55f" , "#5f5" , "#5ff" , "#f55" , "#f5f" , "#ff5" , "#fff"];
+	palette["win7"] = ["#000000" , "#7f7f7f" , "#b97a57" , "#ed1c24" , "#ff7f27" , "#fff200" , "#22b14c" , "#00a2e8" , "#3f48cc" , "#a349a4" ,
+		"#ffffff" , "#c3c3c3" , "#b97a57" , "#ffaec9" , "#ffc90e" , "#efe4b0" , "#b5e61d" , "#99d9ea" , "#7092be" , "#c8bfe7"];
+	palette["gray"] = ["#fff" , "#eee" , "#ddd" , "#ccc" , "#bbb" , "#aaa" , "#999" , "#888" , "#777" , "#666" , "#555" , "#444" , "#333" , "#222" , "#111" , "#000"];
+	palette["feijoa1"] = ["#000" , "#005" , "#00a" , "#00f" , "#050" , "#055" , "#05a" , "#05f" , "#0a0" , "#0a5" , "#0aa" , "#0af" , "#0f0" , "#0f5" , "#0fa" , "#0ff",
+		"#500" , "#505" , "#50a" , "#50f" , "#550" , "#555" , "#55a" , "#55f" , "#5a0" , "#5a5" , "#5aa" , "#5af" , "#5f0" , "#5f5" , "#5fa" , "#5ff",
+		"#a00" , "#a05" , "#a0a" , "#a0f" , "#a50" , "#a55" , "#a5a" , "#a5f" , "#aa0" , "#aa5" , "#aaa" , "#aaf" , "#af0" , "#af5" , "#afa" , "#aff",
+		"#f00" , "#f05" , "#f0a" , "#f0f" , "#f50" , "#f55" , "#f5a" , "#f5f" , "#fa0" , "#fa5" , "#faa" , "#faf" , "#ff0" , "#ff5" , "#ffa" , "#fff"];
+var currentPalette = "cga";
 
 document.addEventListener("DOMContentLoaded", init, false);
 
@@ -40,15 +57,40 @@ function init()
 	dc.fillRect(0, 0, cWidth, cHeight);
 	updateDebugScreen();
 	history[0] = dc.getImageData(0, 0, cWidth, cHeight);
+	
+	document.getElementById("version").innerHTML=infoVersion;
+	document.getElementById("date").innerHTML=infoDate;
 
+	paletteSelect = document.getElementById("palette-select");
+
+	for(tPalette in paletteDesc) {
+		paletteSelect.options[paletteSelect.options.length] = new Option(paletteDesc[tPalette], tPalette);
+	}
+	updatePalette();
+}
+
+function updatePalette() {
+	currentPalette = document.getElementById("palette-select").value;
 	paletteElem = document.getElementById("palette");
 
-	for(tColor in palette) {
+	while(paletteElem.childNodes.length) {
+		paletteElem.removeChild(paletteElem.childNodes[0])
+	}
+
+	var colCount = 0;
+
+	for(tColor in palette[currentPalette]) {
 		var palettine = document.createElement("span");
 		palettine.className = "palettine";
-		palettine.style.background =  palette[tColor];
-		palettine.setAttribute('onclick', 'updateColor("' + palette[tColor] + '");');
+		palettine.style.background =  palette[currentPalette][tColor];
+		palettine.setAttribute('onclick', 'updateColor("' + palette[currentPalette][tColor] + '");');
 		paletteElem.appendChild(palettine);
+		if(colCount == paletteWidth[currentPalette] - 1)
+		{
+			colCount = -1;
+			paletteElem.appendChild(document.createElement("br"));
+		}
+		colCount ++;
 	}
 }
 
@@ -173,6 +215,16 @@ function clearScreen() {
 	//}
 }
 
+function invertColors() {
+	var buffer = history[historyPosition];
+	for (var i = 0; i < buffer.data.length; i += 4)
+		for (var j = 0; j < 3; j++)
+			buffer.data[i + j] = 255 - buffer.data[i + j];
+	 
+	dc.putImageData(buffer, 0, 0);
+	historyOperation(0);
+}
+
 function updateColor(value) {
 	var c=document.getElementById("color"), v=value||c.value;
 	var colorSummary = 0;
@@ -228,6 +280,7 @@ function cHotkeys(event) {
 			case 'X'.charCodeAt(0): historyOperation(2); break;
 			case 'F'.charCodeAt(0): clearScreen(); break;
 			case 'W'.charCodeAt(0): cDrawCancel(); break;
+			case 'I'.charCodeAt(0): invertColors(); break;
 			//default: alert('def')
 		}
 	}
